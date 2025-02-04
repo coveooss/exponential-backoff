099a178a63cb9c1a9d93ab99e68b5e240f178c83
Dan Homola <dan.homola@hotmail.cz>
2025-02-04 22:12:09 +0100

    docs: add TSDoc comments (#68)
    This makes the developer experience a bit nicer: the IDE will display the documentation inline for each of the options and the backOff function itself.
    
    The values in the comments were sourced from the README and only slightly reformatted.

--- src/backoff.ts
+++ src/backoff.ts
@@ -7,6 +7,12 @@ import { DelayFactory } from "./delay/delay.factory";
 
 export { BackoffOptions, IBackOffOptions };
 
+/**
+ * Executes a function with exponential backoff.
+ * @param request the function to be executed
+ * @param options options to customize the backoff behavior
+ * @returns Promise that resolves to the result of the `request` function
+ */
 export async function backOff<T>(
   request: () => Promise<T>,
   options: BackoffOptions = {}
